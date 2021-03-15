be6f9deb1fd7418dde4eb03ff359f0dd17d6b6ac
FarhanJulystio <farhanjt70@gmail.com>
2021-03-15 17:07:07 +0700

    E31192029

---
+++ Minggu 6/application/models/Hello_model.php
@@ -0,0 +1,12 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Farhan","nim"=>"E31192029","gol"=>"C"],
+			["nama"=>"Julystio","nim"=>"E31192255","gol"=>"C"],
+		];
+}
+?>
