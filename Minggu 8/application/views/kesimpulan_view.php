c3fc998eef7375182d30adfc0fafba0a7252fa1c
FarhanJulystio <farhanjt70@gmail.com>
2021-03-29 17:38:33 +0700

    Minggu 8

---
+++ Minggu 8/application/views/kesimpulan_view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title>Kesimpulan</title>
+	<meta name="Author" content=""/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="font-size:18px;">
+    Dari percobaan praktikum maupun Tugas ini dapat disimpulkan yaitu dalam memanggil code data array atau parsing data array dari Code Model ==> Views maka tidak perlu dipanggil satu-satu lagi tiap variabelnya atau setiap index yang ada di code tersebut. <br/>Lalu code itu Cukup saja menggunakan perulangan, <i>dan foreach</i> ini yang kemudian akan berubah ke variabel yang ada di controller dan kemudian dipanggilah index array tersebut.
+</body>
+</html>
